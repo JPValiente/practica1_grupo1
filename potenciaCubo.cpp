7581e5839184d5a3ba68e17f4fff5c7dd2d5b3b8
ricardo mendez <augustoricardo-mendezpum@cunoc.edu.gt>
2020-08-22 11:41:29 -0600

    Funcion cubica

---
+++ potenciaCubo.cpp
@@ -0,0 +1,12 @@
+#include "potenciaCubo.h"
+#include <iostream>
+#include <math.h>
+//funcion para calcular potencia al cubo
+void potenciaCubica()
+{
+    double numero;
+    std::cout<<"Ingrese el digito a elevar ^(3): "<<std::endl;
+    std::cin>>numero;
+    double elevado = pow(numero, 3); //funcion que determina el resultado
+    std::cout << "El numero " << numero << " elevado al cubo es " << elevado;
+}
---
+++ potenciaCubo.h
@@ -0,0 +1,9 @@
+#ifndef POTENCIA_CUBO_H
+#define POTENCIA_CUBO_H
+
+#include <iostream>
+#include <math.h>
+
+void potenciaCubica();
+
+#endif

cdf57ef61a751a8d3ef165ce1c9bca75dbd03dd4
fabricioRG <fabricio21rg@gmail.com>
2020-08-22 13:24:58 -0600

    Arreglo de funcion de division

--- 3.0/Multiplicacionydivision.cpp
+++ 3.0/Multiplicacionydivision.cpp
@@ -11,6 +11,11 @@ int multiplicacion(int a, int b) {
 
 //Funcion que retorna el valor resultado de la division de dos numeros enteros
 int division(int a, int b) {
-    cout<<"El resultado es: "<<a/b<<endl;
-    return a/b;
+	if(b != 0){
+		cout<<"El resultado es: "<<a/b<<endl;
+    	return a/b;
+	} else {
+		cout<<"Error: la division entre 0 no existe"<<endl;
+		return 0;
+	}
 }
